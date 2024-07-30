ab49ccabe876f032b157dd69c637d54dd15d4d93
tayqiter00 <tayqiter00@gmail.com>
2024-07-31 01:20:17 +0800

    split long function

--- main.c
+++ main.c
@@ -3,53 +3,69 @@
 void    usage(void)
 {
     dprintf(STDERR_FILENO, "Error: Improper arguments!\n");
-    printf("Example: ./pipex filein cmd1 cmd2 <...> fileout");
+    printf("Example: ./pipex filein cmd1 cmd2 <...> fileout\n");
     exit(EXIT_FAILURE);
 }
 
-void    pipe_n_exec(char *cmd, char **envp)
-{
-    int fds[2];
-    pid_t   pid;
+// void    pipe_n_exec(char *cmd, char **envp)
+// {
+//     int fds[2];
+//     pid_t   pid;
 
-    if (pipe(fds))
-    {
-        perror("Pipe error!\n");
-        exit(EXIT_FAILURE);
-    }
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("Fork error!\n");
-        exit(EXIT_FAILURE);
-    }
-    if (pid == 0)
-    {
-        close(fds[R_END]);
-        dup2(fds[W_END], STDOUT_FILENO);
-    }
+//     if (pipe(fds))
+//     {
+//         perror("Pipe error!\n");
+//         exit(EXIT_FAILURE);
+//     }
+//     pid = fork();
+//     if (pid == -1)
+//     {
+//         perror("Fork error!\n");
+//         exit(EXIT_FAILURE);
+//     }
+//     if (pid == 0)
+//     {
+//         close(fds[R_END]);
+//         dup2(fds[W_END], STDOUT_FILENO);
+//         close(fds[W_END]);
+//         execute();
+//     }
+//     else
+//     {
+//         waitpid(-1, NULL, 0);
+//         close(fds[W_END]);
+//     }
     
-    // execve(cmd, cmd);
+//     // execve(cmd, cmd);
+// }
+
+static void set_normal_iostream(int fileio_fds[2], char *filein, char *fileout)
+{
+    fileio_fds[0] = open(filein, O_RDONLY, 0400);
+    if (fileio_fds[0] == -1)
+    {
+        perror("Filein cannot be opened!\n");
+        exit(EXIT_FAILURE);
+    }
+    dup2(fileio_fds[0], STDIN_FILENO);
+    close(fileio_fds[0]);
+    fileio_fds[0] = -1; // set to -1 to indicate it's no longer used
+    fileio_fds[1] = open(fileout, O_CREAT | O_WRONLY | O_TRUNC, 0755); // can probably be set later too
 }
 
 int main(int ac, char **av, char **envp)
 {
     int i;
-    int filein;
-    int fileout;
+    int fileio_fds[2]; // fd_filein and fd_fileout
 
     if (ac <= 4)
         usage();
-    filein = open(av[1], O_RDONLY, 0400);
-    if (filein == -1)
-    {
-        perror("Filein cannot be opened!\n");
-        exit(EXIT_FAILURE);
-    }
-    dup2(filein, STDIN_FILENO);
-    close(filein);
-    fileout = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0755);
-    // i = 3;
+    // if (strncmp(av[1], "here_doc", strlen("here_doc")) == 0)
+    // {
+
+    // }
+    // else
+        set_normal_iostream(fileio_fds, av[1], av[ac - 1]);
     // while (ac - 4)
     // {
     //     pipe_n_exec(av[i++], envp);
--- pipex.h
+++ pipex.h
@@ -6,6 +6,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <string.h>
 
 # define R_END 0
 # define W_END 1
